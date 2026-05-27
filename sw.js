f66adbc4541d6c3089cfb9290f6dba48dd034ea8
betioli <williambetioli@hotmail.com>
2026-05-27 13:13:03 -0300

    Nova logo redimensionada v=2.3.1

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "alyara-site-v2.3";
+const CACHE_NAME = "alyara-site-v2.3.1";
 const CORE_ASSETS = [
     "/",
     "/index.html",
-    "/styles.css?v=2.3",
-    "/script.js?v=2.3",
+    "/styles.css?v=2.3.1",
+    "/script.js?v=2.3.1",
     "/robots.txt",
     "/sitemap.xml",
     "/image/fachada-de-ripado.webp",
